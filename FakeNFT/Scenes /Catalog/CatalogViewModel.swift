2fd033d1269bdeb722b9d3ada903cde074d56d4e
Konstantin Penzin <koverboard@icloud.com>
2024-01-28 18:18:28 +0500

    Store sort order for catalog

--- FakeNFT/Scenes /Catalog/CatalogViewModel.swift
+++ FakeNFT/Scenes /Catalog/CatalogViewModel.swift
@@ -12,11 +12,21 @@ final class CatalogViewModel {
     let serviceAssembly: ServicesAssembly
     var alertPresenter: AlertPresenterProtocol?
 
+    private let defaults = UserDefaults.standard
+    private var sortOrder: SortOrder? {
+        didSet {
+            defaults.set(sortOrder?.rawValue, forKey: "sortOrder")
+        }
+    }
+
     @Observable
     private(set) var collections: [NftCollection] = []
 
     init(serviceAssembly: ServicesAssembly) {
         self.serviceAssembly = serviceAssembly
+        if let order = defaults.string(forKey: "sortOrder") {
+            sortOrder = SortOrder.init(rawValue: order)
+        }
         loadCollections()
     }
 
@@ -25,7 +35,14 @@ final class CatalogViewModel {
         serviceAssembly.catalogService.getCollections { [weak self] result in
             switch result {
             case .success(let items):
-                self?.collections = items
+                switch self?.sortOrder {
+                case .name:
+                    self?.collections = items.sorted(by: { $0.name < $1.name })
+                case .nfts:
+                    self?.collections = items.sorted(by: { $0.nfts.count < $1.nfts.count })
+                case nil:
+                    self?.collections = items
+                }
             case .failure:
                 self?.showNetworkError {
                     self?.loadCollections(completion: completion)
@@ -38,10 +55,12 @@ final class CatalogViewModel {
 
     func sortCollectionsByName() {
         collections = collections.sorted(by: { $0.name < $1.name })
+        sortOrder = SortOrder.name
     }
 
     func sortCollectionsByCount() {
         collections = collections.sorted(by: { $0.nfts.count < $1.nfts.count })
+        sortOrder = SortOrder.nfts
     }
 }
 private extension CatalogViewModel {
